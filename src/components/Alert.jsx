b0d5b4d4d7351890deb39d775223322665c839a1
Alex Kondratyuk <lynxtaa@gmail.com>
2017-09-30 10:42:42 +0300

    use native portals

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import withPortal from '../HOCs/withPortal'
+import Portal from './Portal'
 
 const Alert = ({ children, onClick }) => (
-	<div className="container alert alert-warning alert-dismissible fade show" role="alert">
-		<button type="button" className="close" onClick={onClick} aria-label="Close">
-			<span aria-hidden="true">&times;</span>
-		</button>
-		{ children }
-	</div>
+	<Portal id="alerts">
+		<div className="container alert alert-warning alert-dismissible fade show" role="alert">
+			<button type="button" className="close" onClick={onClick} aria-label="Close">
+				<span aria-hidden="true">&times;</span>
+			</button>
+			{ children }
+		</div>
+	</Portal>
 )
 
 Alert.propTypes = {
@@ -16,4 +18,4 @@ Alert.propTypes = {
 	onClick: PropTypes.func.isRequired,
 }
 
-export default withPortal('alerts')(Alert)
+export default Alert
---
+++ src/components/Portal.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+
+class Portal extends React.Component {
+	constructor(props) {
+		super(props)
+
+		const { id } = this.props
+		let portal = id && document.getElementById(id)
+
+		if (!portal) {
+			portal = document.createElement('div')
+			portal.id = id
+			document.body.insertBefore(portal, document.body.firstChild)
+		}
+
+		this.portal = portal
+	}
+
+	componentWillUnmount() {
+		document.body.removeChild(this.portal)
+	}
+
+	render() {
+		return ReactDOM.createPortal(this.props.children, this.portal)
+	}
+}
+
+Portal.propTypes = {
+	id: PropTypes.string.isRequired,
+	children: PropTypes.node.isRequired,
+}
+
+export default Portal
