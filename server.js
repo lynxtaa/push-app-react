323ac853cd379021f2cfacf9d1dbdeb89bca93c0
Alex Kondratyuk <lynxtaa@gmail.com>
2017-11-04 19:27:35 +0300

    refactor server code

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 const express = require('express')
 const {join} = require('path')
 
-const PORT = 8080
-const distFolder = 'dist'
+const PORT = process.env.PORT || 8080
+const distFolder = join(__dirname, 'dist')
 
 express()
 	.use(express.static(distFolder))
-	.get('*', (req, res) => {
-		res.sendFile(join(__dirname, distFolder, 'index.html'))
-	})
-	.listen(PORT, err => {
+	.listen(PORT, err => err ?
+		console.log(err) :
 		console.log(`Server started on: http://localhost:${PORT}`)
-		if (err) {
-			console.log(err)
-		}
-	})
+	)
