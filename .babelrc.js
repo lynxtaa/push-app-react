b2b69556dd0e3f4deea47767e095385237204dec
Alex Kondratyuk <lynxtaa@gmail.com>
2018-05-01 16:49:21 +0300

    use babel-preset-react-app

--- .babelrc.js
+++ .babelrc.js
@@ -1,17 +1,8 @@
-const presets = [
-	['@babel/env', { useBuiltIns: 'entry', modules: false }],
-	['@babel/react', { development: process.env.NODE_ENV == 'development' }],
-]
-
-const plugins = [
-	['@babel/proposal-object-rest-spread', { useBuiltIns: true }],
-	['@babel/plugin-proposal-class-properties', { loose: true }],
-]
+const presets = ['react-app']
+const plugins = []
 
 if (process.env.NODE_ENV == 'development') {
 	plugins.push('react-hot-loader/babel')
-} else if (process.env.NODE_ENV == 'production') {
-	plugins.push(['transform-react-remove-prop-types', { removeImport: true }])
 }
 
 module.exports = { presets, plugins }
