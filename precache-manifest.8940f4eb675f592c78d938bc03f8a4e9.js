0e087366abc7764abce16e4f27cd4308ce1c9018
Alex Kondratyuk <lynxtaa@gmail.com>
2020-01-20 22:38:56 +0300

    Updates

---
+++ precache-manifest.8940f4eb675f592c78d938bc03f8a4e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30694b57f0f81983146e28bf0c827cc5",
+    "url": "/push-app-react/index.html"
+  },
+  {
+    "revision": "ca5860a0d36af6928810",
+    "url": "/push-app-react/static/css/main.b412de70.chunk.css"
+  },
+  {
+    "revision": "d41dcad45b4fd5815185",
+    "url": "/push-app-react/static/js/2.02c63da5.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/push-app-react/static/js/2.02c63da5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ca5860a0d36af6928810",
+    "url": "/push-app-react/static/js/main.3d7ebbec.chunk.js"
+  },
+  {
+    "revision": "bbc362bb2219ac9d1e60",
+    "url": "/push-app-react/static/js/runtime-main.200372a3.js"
+  }
+]);
