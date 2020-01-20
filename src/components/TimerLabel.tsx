16bd67b7e7ea2641bb479cf839c367293d35cf71
Alex Kondratyuk <lynxtaa@gmail.com>
2020-01-20 23:33:04 +0300

    feat: animate timer

--- src/components/TimerLabel.tsx
+++ src/components/TimerLabel.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import { animated, useSpring } from 'react-spring'
 
 interface Props {
 	children: React.ReactNode
 	onClick(event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void
 }
 
-const TimerLabel = ({ children, onClick }: Props) => (
-	<button className="btn btn-outline-info" onClick={onClick} type="button">
-		{children}
-	</button>
-)
+const round = (value: any) => value.interpolate((value: number) => Math.round(value))
+
+const TimerLabel = ({ children, onClick }: Props) => {
+	const { number } = useSpring({ number: children })
+
+	return (
+		<animated.button className="btn btn-outline-info" onClick={onClick} type="button">
+			{round(number)}
+		</animated.button>
+	)
+}
 
 export default TimerLabel
