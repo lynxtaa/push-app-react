8aead5c04d8c06b3e87f608b21c6182d06a5964d
Alex Kondratyuk <lynxtaa@gmail.com>
2020-09-05 08:50:51 +0300

    chore: use focus-visible for outlines

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { ThemeProvider, CSSReset, ColorModeProvider } from '@chakra-ui/core'
+import { Global, css } from '@emotion/core'
+
+import 'focus-visible/dist/focus-visible.min.js'
 
 import AppRouter from './AppRouter'
 import theme from './theme'
@@ -8,6 +11,14 @@ import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
 	<ThemeProvider theme={theme}>
+		<Global
+			styles={css`
+				.js-focus-visible :focus:not([data-focus-visible-added]) {
+					outline: none;
+					box-shadow: none;
+				}
+			`}
+		/>
 		<ColorModeProvider>
 			<CSSReset />
 			<AppRouter />
